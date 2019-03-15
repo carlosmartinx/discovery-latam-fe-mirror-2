42fe830ee4b499d9e89ef84b74add5d5bf32b0d0
Daniela Salazar <danii.salazar00@gmail.com>
2019-03-15 00:00:02 -0500

    Adding theme

--- modules/core/src/styles/mainTheme.js
+++ modules/core/src/styles/mainTheme.js
@@ -1,16 +1,12 @@
 const globalColors = require('./colors');
+const globalLayout = require('./layout');
 
 export default {
-  breakpoints: ['414px', '768px', '1024px', '1440px'],
+  breakpoints: globalLayout.default,
   fontSizes: [12, 14, 16, 20, 24, 32, 48, 64],
-  colors: globalColors,
+  colors: globalColors.default,
   space: [0, 8, 16, 24, 32, 40, 48],
   fonts: {
     sans: 'Fira Sans, sans-serif',
-    mono: 'Menlo, monospace',
-  },
-  shadows: {
-    small: '0 0 4px rgba(0, 0, 0, .125)',
-    large: '0 0 24px rgba(0, 0, 0, .125)',
   },
 };
