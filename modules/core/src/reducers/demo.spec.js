b489747e1eea67cb18143f3ae2c28a0d7ec2061d
julian mosquera <julian.mosquera@innovativefreightalgorithms.com>
2019-03-16 11:57:39 -0500

    tests fixed intl done

--- modules/core/src/reducers/demo.spec.js
+++ modules/core/src/reducers/demo.spec.js
@@ -17,4 +17,18 @@ describe('test reducer', () => {
     };
     expect(reducer(undefined, { type: TYPES.TEST.SET.REQUEST })).toEqual(expectedCase);
   });
+
+  it('should handle TYPES.TEST.SET.SUCCESS', () => {
+    const data = 'test';
+    const expectedCase = {
+      ...initialState,
+      loading: false,
+      test: data,
+    };
+    const action = {
+      type: TYPES.TEST.SET.SUCCESS,
+      payload: { data },
+    };
+    expect(reducer(undefined, action)).toEqual(expectedCase);
+  });
 });
