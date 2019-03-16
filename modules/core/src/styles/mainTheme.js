590da2d0c5f569176333d13a60cd30d16050f5bf
Daniela Salazar <salazardaniela@users.noreply.github.com>
2019-03-15 21:26:14 -0500

    Feature/theme (#3)
    * Adding generic variables and theme
    
    * Adding theme
    
    * Fix color names

--- modules/core/src/styles/mainTheme.js
+++ modules/core/src/styles/mainTheme.js
@@ -1,17 +1,12 @@
+const globalColors = require('./colors');
+const globalLayout = require('./layout');
+
 export default {
-  breakpoints: ['40em', '52em', '64em'],
+  breakpoints: globalLayout.default,
   fontSizes: [12, 14, 16, 20, 24, 32, 48, 64],
-  colors: {
-    blue: '#07c',
-    lightgray: '#f6f6ff',
-  },
-  space: [0, 4, 8, 16, 32, 64, 128, 256],
+  colors: globalColors.default,
+  space: [0, 8, 16, 24, 32, 40, 48],
   fonts: {
-    sans: 'system-ui, sans-serif',
-    mono: 'Menlo, monospace',
-  },
-  shadows: {
-    small: '0 0 4px rgba(0, 0, 0, .125)',
-    large: '0 0 24px rgba(0, 0, 0, .125)',
+    sans: 'Fira Sans, sans-serif',
   },
 };
