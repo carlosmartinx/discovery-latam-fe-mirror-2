49dfc0afc980f6062817162fce27c878bf6d6281
Wilder Villarraga <hormigah@gmail.com>
2019-03-27 20:23:48 +1000

    component SliderSlick was updated with general styles

--- modules/shared-components/src/slider-slick/styled.js
+++ modules/shared-components/src/slider-slick/styled.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import { Box } from 'rebass';
 import styled from 'styled-components';
+import get from 'lodash/get';
 
 export const BoxSlider = styled(Box)`
   .slick-prev {
+    background-image: url(${props => get(props, 'theme.icons.slidearrowleftfilled', '')});
     left: 0;
+    &:hover, &:focus {
+      background-image: url(${props => get(props, 'theme.icons.slidearrowleftfilled', '')});
+    }
   }
   .slick-next {
+    background-image: url(${props => get(props, 'theme.icons.slidearrowrightfilled', '')});
     right: 0;
+    &:hover, &:focus {
+      background-image: url(${props => get(props, 'theme.icons.slidearrowrightfilled', '')});
+    }
+  }
+  .slick-prev, .slick-next {
+    background-repeat: no-repeat;
+    &.slick-disabled {
+      opacity: 0;
+    }
+    &:before {
+      display: none;
+    }
+    &:hover, &:focus {
+      background-repeat: no-repeat;
+    }
   }
 `;
