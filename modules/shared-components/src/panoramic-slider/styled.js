9f9769dd8a7df4a378aeee96adf6a24a9bb18301
Adriana Bermudez <adriana.bermudez@tecno-makers.com>
2019-04-02 21:49:48 -0500

    Adding variables fallback

--- modules/shared-components/src/panoramic-slider/styled.js
+++ modules/shared-components/src/panoramic-slider/styled.js
@@ -9,12 +9,12 @@ import {
 } from '../content-card/styled';
 
 const Panoramic = styled(Box)`
-  max-width: ${props => get(props, 'theme.breakpoints.3', '')};
+  max-width: ${props => get(props, 'theme.breakpoints.3', '1440px')};
   margin: 0 auto;
   position: relative;
 
   .slick-dots {
-    bottom: ${props => get(props, 'theme.space[1]', '')}px;
+    bottom: ${props => get(props, 'theme.space[1]', '8')}px;
 
     & li {
       margin: 0;
@@ -32,7 +32,7 @@ const Panoramic = styled(Box)`
     }
     & .slick-active {
         & button {
-          background: ${props => get(props, 'theme.colors.dPrimary', '')};
+          background: ${props => get(props, 'theme.colors.dPrimary', '#246ad3')};
         }
     }
   }
@@ -50,71 +50,71 @@ const Panoramic = styled(Box)`
 
   & .slick-next {
     background-size: 100%;
-    right: ${props => get(props, 'theme.space[1]', '')}px;
+    right: ${props => get(props, 'theme.space[1]', '8px')}px;
 
     &:hover, &:focus {
       background-size: 100%;
       opacity: 0.8;
     }
 
-    @media (min-width: ${props => get(props, 'theme.breakpoints.3', '')}) {
+    @media (min-width: ${props => get(props, 'theme.breakpoints.3', '1440px')}) {
      right: 98px
     }
   }
 
   & .slick-prev {
     background-size: 100%;
-    left: ${props => get(props, 'theme.space[1]', '')}px;
+    left: ${props => get(props, 'theme.space[1]', '8')}px;
 
     &:hover, &:focus {
       background-size: 100%;
       opacity: 0.8;
     }
-    @media (min-width: ${props => get(props, 'theme.breakpoints.3', '')}) {
+    @media (min-width: ${props => get(props, 'theme.breakpoints.3', '1440px')}) {
       left: 98px
     }
   }
 
   ${ContentWrapper} {
     background: rgba(0,37,91,0.85);
-    border-bottom: 3px solid ${props => get(props, 'theme.colors.dPrimary', '')}
-    bottom: ${props => get(props, 'theme.space[0]', '')}px;
-    left: ${props => get(props, 'theme.space[0]', '')}px;
+    border-bottom: 3px solid ${props => get(props, 'theme.colors.dPrimary', '#246ad3')}
+    bottom: ${props => get(props, 'theme.space[0]', '0')}px;
+    left: ${props => get(props, 'theme.space[0]', '0')}px;
     margin: auto;
-    padding: ${props => get(props, 'theme.space[1]', '')}px 
-    ${props => get(props, 'theme.space[3]', '')}px
-    ${props => get(props, 'theme.space[5]', '')}px;
+    padding: ${props => get(props, 'theme.space[1]', '8')}px 
+    ${props => get(props, 'theme.space[3]', '32')}px
+    ${props => get(props, 'theme.space[5]', '40')}px;
     position: absolute;
-    right: ${props => get(props, 'theme.space[0]', '')}px;
+    right: ${props => get(props, 'theme.space[0]', '0')}px;
 
-    @media (min-width: ${props => get(props, 'theme.breakpoints.3', '')}) {
-      padding: ${props => get(props, 'theme.space[1]', '')}px 98px
-      ${props => get(props, 'theme.space[5]', '')}px;
+    @media (min-width: ${props => get(props, 'theme.breakpoints.3', '1440px')}) {
+      padding: ${props => get(props, 'theme.space[1]', '8')}px 98px
+      ${props => get(props, 'theme.space[5]', '40')}px;
     }
   }
 
   ${HeadlineWrapper} {
     display: flex;
     align-items: center;
-    padding-bottom: ${props => get(props, 'theme.space[1]', '')}px
+    padding-bottom: ${props => get(props, 'theme.space[1]', '8')}px
   }
 
   ${ChannelLogo} {
-    border-right: 1px solid ${props => get(props, 'theme.colors.white', '')}
-    padding: ${props => get(props, 'theme.space[1]', '')}px
-    ${props => get(props, 'theme.space[3]', '')}px
-    ${props => get(props, 'theme.space[1]', '')}px 0;
-    margin-right: ${props => get(props, 'theme.space[3]', '')}px;
+    border-right: 1px solid ${props => get(props, 'theme.colors.white', '#ffffff')}
+    padding: ${props => get(props, 'theme.space[1]', '8')}px
+    ${props => get(props, 'theme.space[3]', '24')}px
+    ${props => get(props, 'theme.space[1]', '8')}px 0;
+    margin-right: ${props => get(props, 'theme.space[3]', '24')}px;
     opacity: 1;
   }
 
   ${Title} {
-    font-size: ${props => get(props, 'theme.fontSizes[5]', '')}px;
+    font-size: ${props => get(props, 'theme.fontSizes[5]', '28')}px;
     line-height: 36px;
     margin: 0;
 
-    @media (min-width: ${props => get(props, 'theme.breakpoints.2', '')}) {
-      font-size: ${props => get(props, 'theme.fontSizes[8]', '')}px;
+    @media (min-width: ${props => get(props, 'theme.breakpoints.2', '1024px')}) {
+      font-size: ${props => get(props, 'theme.fontSizes[8]', '44')}px;
       line-height: 52px;
     }
   }
