dab059d314c7272f6ba6623071596a0040411954
adrianabermudez <adriana.bermudez@tecno-makers.com>
2019-04-04 22:54:07 -0500

    Adding content from Home endpoint

--- modules/core/src/pages/Home/index.js
+++ modules/core/src/pages/Home/index.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Box, Flex } from 'rebass';
+import { get } from 'lodash';
 
 import { esCodes } from '@discovery-web-app/shared-components/config/tagCodes';
 
@@ -11,6 +12,7 @@ import {
   Carousel,
   CookieBar,
   SliderContentCard,
+  PanoramicSlider,
   Ads,
 } from '@discovery-web-app/shared-components';
 
@@ -23,133 +25,22 @@ import Page from '../../components/page';
 
 const Home = (props) => {
   const { appState } = props;
-
   useEffect(() => {
     props.getHomeAction();
   }, []);
 
-  global.console.log(appState.home);
-
-  const items = [
-    {
-      title: 'Comida por machete',
-      type: 'image',
-      duration: {
-        minutes: '1',
-        seconds: '55',
-      },
-      description: 'Dani se siente hambrienta y apartada de su nuevo grupo, así que decide explorar los alrededores, encontrando nuevos amigos con los que podrá negociar comida por el uso de su machete.',
-      preview_image: {
-        image_1_1: 'https://cdn.admin-tudiscovery.com/sites/default/files/styles/slider_mobile/public/2018-12/Comida%20por%20machete.jpeg?itok=sNzT-afs',
-        image_url: 'https://cdn.admin-tudiscovery.com/sites/default/files/styles/destacados/public/2018-12/Comida%20por%20machete.jpeg?itok=CJtDQgD-',
-        image_alt: 'Comida por machete',
-      },
-      url: '/discovery/supervivencia-al-desnudo-edicion-extrema/video/comida-por-machete',
-      categories: [{
-        name: 'aventura',
-        url: '/temas/aventura',
-      }, {
-        name: 'supervivencia',
-        url: '/temas/supervivencia',
-      }],
-      channel: {
-        name: 'Discovery',
-        logo: 'https://cdn.admin-tudiscovery.com/sites/default/files/2018-12/Discovery-Logo-white.png',
-        url: '/discovery',
-        logo_svg: 'https://cdn.admin-tudiscovery.com/sites/default/files/logos/discovery.svg',
-      },
-    }, {
-      title: 'Creando un parque temático para niños',
-      region: ['Latam'],
-      fecha: '1543597509',
-      duration: {
-        minutes: '1',
-        seconds: '55',
-      },
-      type: 'video',
-      description: 'Tyson y su equipo reciben un encargo de la compañía International Princess World, un parque temático para niños que será presentado en China.',
-      preview_image: {
-        image_1_1: 'https://cdn.admin-tudiscovery.com/sites/default/files/styles/slider_mobile/public/2EC4E30229B343DFA43DE87F56F1838D.jpg?itok=NphyXaO9', image_url: 'https://cdn.admin-tudiscovery.com/sites/default/files/styles/destacados/public/2EC4E30229B343DFA43DE87F56F1838D.jpg?itok=WCZhnBNj', image_file: '2EC4E30229B343DFA43DE87F56F1838D.jpg', image_alt: 'Creando un parque temático para niños',
-      },
-      url: '/discoveryhh/casitas-para-jugar/video/creando-un-parque-tematico-para-ninos',
-      categories: [{ name: 'hogar', url: '/temas/hogar' }, { name: 'jardim', url: '/temas/jardin' }],
-      channel: {
-        name: 'DiscoveryHH',
-        logo: 'https://cdn.admin-tudiscovery.com/sites/default/files/2018-12/HH-Logo-white.png',
-        url: '/discoveryhh',
-        codigos: {
-          Latam: 'DHLA-SP', Brasil: 'DHBR-PRT', Mexico: 'DHME-SP', Argentina: 'DHAR-SP', Venezuela: 'DHTV-SP',
-        },
-        codes: [{ Latam: 'DSLA-SP' }, { Brasil: 'DCBR-PRT' }, { Mexico: 'DCME-SP' }, { Argentina: 'DCAR-SP' }, { Venezuela: 'DCVE-SP' }, { Latam: 'TULA-SP' }, { Brasil: 'THDB-PRT' }, { Latam: 'APLA-SP' }, { Brasil: 'APBR-PRT' }, { Argentina: 'APAR-SP' }, { Latam: 'DHLA-SP' }, { Brasil: 'DHBR-PRT' }, { Mexico: 'DHME-SP' }, { Argentina: 'DHAR-SP' }, { Venezuela: 'DHTV-SP' }],
-        logo_svg: 'https://cdn.admin-tudiscovery.com/sites/default/files/logos/hh.svg',
-      },
-    }, {
-      title: 'Comida por machete',
-      type: 'image',
-      duration: {
-        minutes: '1',
-        seconds: '55',
-      },
-      description: 'Dani se siente hambrienta y apartada de su nuevo grupo, así que decide explorar los alrededores, encontrando nuevos amigos con los que podrá negociar comida por el uso de su machete.',
-      preview_image: {
-        image_1_1: 'https://cdn.admin-tudiscovery.com/sites/default/files/styles/slider_mobile/public/2018-12/Comida%20por%20machete.jpeg?itok=sNzT-afs',
-        image_url: 'https://cdn.admin-tudiscovery.com/sites/default/files/styles/destacados/public/2018-12/Comida%20por%20machete.jpeg?itok=CJtDQgD-',
-        image_alt: 'Comida por machete',
-      },
-      url: '/discovery/supervivencia-al-desnudo-edicion-extrema/video/comida-por-machete',
-      categories: [{
-        name: 'aventura',
-        url: '/temas/aventura',
-      }, {
-        name: 'supervivencia',
-        url: '/temas/supervivencia',
-      }],
-      channel: {
-        name: 'Discovery',
-        logo: 'https://cdn.admin-tudiscovery.com/sites/default/files/2018-12/Discovery-Logo-white.png',
-        url: '/discovery',
-        logo_svg: 'https://cdn.admin-tudiscovery.com/sites/default/files/logos/discovery.svg',
-      },
-    }, {
-      title: 'Creando un parque temático para niños',
-      region: ['Latam'],
-      fecha: '1543597509',
-      duration: {
-        minutes: '1',
-        seconds: '55',
-      },
-      type: 'video',
-      description: 'Tyson y su equipo reciben un encargo de la compañía International Princess World, un parque temático para niños que será presentado en China.',
-      preview_image: {
-        image_1_1: 'https://cdn.admin-tudiscovery.com/sites/default/files/styles/slider_mobile/public/2EC4E30229B343DFA43DE87F56F1838D.jpg?itok=NphyXaO9', image_url: 'https://cdn.admin-tudiscovery.com/sites/default/files/styles/destacados/public/2EC4E30229B343DFA43DE87F56F1838D.jpg?itok=WCZhnBNj', image_file: '2EC4E30229B343DFA43DE87F56F1838D.jpg', image_alt: 'Creando un parque temático para niños',
-      },
-      url: '/discoveryhh/casitas-para-jugar/video/creando-un-parque-tematico-para-ninos',
-      categories: [{ name: 'hogar', url: '/temas/hogar' }, { name: 'jardim', url: '/temas/jardin' }],
-      channel: {
-        name: 'DiscoveryHH',
-        logo: 'https://cdn.admin-tudiscovery.com/sites/default/files/2018-12/HH-Logo-white.png',
-        url: '/discoveryhh',
-        codigos: {
-          Latam: 'DHLA-SP', Brasil: 'DHBR-PRT', Mexico: 'DHME-SP', Argentina: 'DHAR-SP', Venezuela: 'DHTV-SP',
-        },
-        codes: [{ Latam: 'DSLA-SP' }, { Brasil: 'DCBR-PRT' }, { Mexico: 'DCME-SP' }, { Argentina: 'DCAR-SP' }, { Venezuela: 'DCVE-SP' }, { Latam: 'TULA-SP' }, { Brasil: 'THDB-PRT' }, { Latam: 'APLA-SP' }, { Brasil: 'APBR-PRT' }, { Argentina: 'APAR-SP' }, { Latam: 'DHLA-SP' }, { Brasil: 'DHBR-PRT' }, { Mexico: 'DHME-SP' }, { Argentina: 'DHAR-SP' }, { Venezuela: 'DHTV-SP' }],
-        logo_svg: 'https://cdn.admin-tudiscovery.com/sites/default/files/logos/hh.svg',
-      },
-    },
-  ];
-
   const esContent = (
     <p>
-  Al acceder a nuestro sitio, usted entiende y acepta nuestros Términos y Condiciones.
-  Como la mayoría de los sitios web, los sitios de Discovery utilizan cookies
-  para mejorar su experiencia con nuestro contenido.
-  Para obtener más información acerca de las cookies y cómo administrarlas,
-  por favor lea nuestra Política de privacidad.
-  Declaro que he leído y acepto los
+      Al acceder a nuestro sitio, usted entiende y acepta nuestros Términos y Condiciones.
+      Como la mayoría de los sitios web, los sitios de Discovery utilizan cookies
+      para mejorar su experiencia con nuestro contenido.
+      Para obtener más información acerca de las cookies y cómo administrarlas,
+      por favor lea nuestra Política de privacidad.
+      Declaro que he leído y acepto los
       {' '}
       <a href="https://tudiscovery.com/terminos-y-condiciones" rel="noopener noreferrer" target="_blank"> términos y condiciones</a>
       {' '}
-  y las
+      y las
       {' '}
       <a href="políticas de privacidad" rel="noopener noreferrer" target="_blank"> políticas de privacidad </a>
     </p>
@@ -157,18 +48,21 @@ const Home = (props) => {
 
   const unitTextEs = 'discoverylatam/';
   const unitText = `${unitTextEs}Mobile_leaderboard_bottom`;
-
   return (
     <Page id="homepage" background="#001534">
       <Box>
+        {appState.home.slider && <PanoramicSlider sliderContent={appState.home.slider} />}
+      </Box>
+      <Box>
         <BulletTitle barcolor="default" backtheme="dark" titleContent="Destacados" />
-        <Carousel sliderContent={items} />
+        {appState.home.destacados && <Carousel sliderContent={appState.home.destacados} />}
       </Box>
       <Box>
         <BulletTitle barcolor="default" backtheme="dark" titleContent="Recomendados" />
         <Flex flexDirection={['column', 'column', 'row', 'row']} alignItems="center">
           <Box width={[1, 1, 8 / 12]}>
-            <SliderContentCard sliderContent={items} />
+            {appState.home.recommended
+            && <SliderContentCard sliderContent={appState.home.recommended} />}
           </Box>
           <Box width={[1, 1, 4 / 12]}>
             <Ads
@@ -185,15 +79,18 @@ const Home = (props) => {
       </Box>
       <Box>
         <BulletTitle barcolor="default" backtheme="dark" titleContent="Entretenimiento real" />
-        <Carousel sliderContent={items} />
+        {appState.home.categories
+        && <Carousel sliderContent={get(appState.home, 'categories[0].nodes')} />}
       </Box>
       <Box>
         <BulletTitle barcolor="default" backtheme="dark" titleContent="Estilo de vida" />
-        <Carousel sliderContent={items} />
+        {appState.home.categories
+        && <Carousel sliderContent={get(appState.home, 'categories[1].nodes')} />}
       </Box>
       <Box>
         <BulletTitle barcolor="default" backtheme="dark" titleContent="DISCOVERY K!DS PLAY!" />
-        <Carousel sliderContent={items} />
+        {appState.home.categories
+        && <Carousel sliderContent={get(appState.home, 'categories[2].nodes')} />}
       </Box>
       <CookieBar legal={esContent} btnText="Aceptar" />
     </Page>
